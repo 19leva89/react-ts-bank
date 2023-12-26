cb648cf3901d8bacb1539b24ca34e65b78f16d0b
Dmitry <19leva89@gmail.com>
2023-12-27 00:48:32 +0200

    upd project

--- client/src/components/button/index.tsx
+++ client/src/components/button/index.tsx
@@ -1,28 +1,20 @@
 import { FC } from "react";
 import "./style.css";
-import btnImg from "./btn-ua-parcels.svg";
 
 type ButtonProps = {
   name: string;
   className: string;
-  btnHeight: string;
   onClick?: () => void;
 };
 
-export const Button: FC<ButtonProps> = ({ name, className, btnHeight, onClick }) => {
+export const Button: FC<ButtonProps> = ({ name, className, onClick }) => {
   const handleClick = () => {
     console.log("Button clicked");
     // Ви можете виконати інші дії, що пов'язані з натисканням кнопки тут
   };
 
   return (
-    <button
-      type="submit"
-      className={className}
-      style={{ height: btnHeight }}
-      onClick={onClick || handleClick}
-    >
-      {btnImg && <img height="15px" src={btnImg} alt="Button" />}
+    <button type="submit" className={className} onClick={onClick || handleClick}>
       <span className="header__btn--info">{name}</span>
     </button>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import { FC } from "react";
+import { Button } from "../components/button";
+import background from "./../img/background.png";
+import backgroundSafe from "./../img/background-safe.png";
 
 const WelcomePage: FC = () => {
-  return <main>Home</main>;
+  return (
+    <main>
+      <img className="background-img" src={background} alt="Background" />
+
+      <section className="wrapper">
+        <h1 className="wrapper__title">Hello!</h1>
+        <p className="wrapper__text">Welcome to bank app</p>
+
+        <img className="background-safe" src={backgroundSafe} alt="Background safe" />
+
+        <Button name="Register" className="button button__register" onClick={undefined} />
+        <Button name="Login" className="button button__login" onClick={undefined} />
+      </section>
+    </main>
+  );
 };
 
 export default WelcomePage;
